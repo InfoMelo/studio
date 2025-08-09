4f5b7bb5921d94ea726cb425c2e0533c4bc6e09b
Info Meloy <meloyinfo@gmail.com>
2025-08-09 11:44:35 +0000

    coba terapkan keseluruh halaman namun tetap pertahankan warna utama

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
   return (
     <>
       <TopBar />
-      <header className="sticky top-0 z-50 w-full border-b bg-[#F0F8FF]/95 backdrop-blur supports-[backdrop-filter]:bg-[#F0F8FF]/60">
+      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container px-4 md:px-6 flex h-16 items-center justify-between">
           <div className="flex items-center gap-3">
             <Link href="/" className="flex items-center gap-3">
