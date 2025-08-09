7048a3b8d8eb604803e512b7000682bd54f2c101
Info Meloy <meloyinfo@gmail.com>
2025-08-09 14:26:05 +0000

    gambar kenyamanan pasien pada section header ganti dengan https://res.cl

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -27,7 +27,7 @@ const heroSlides = [
 const whyUsItems = (t: (key: string) => string) => [
     { title: t('profesionalTitle'), desc: t('profesionalDesc'), imageUrl: 'https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', aiHint: 'doctors discussing' },
     { title: t('teknologiTitle'), desc: t('teknologiDesc'), imageUrl: 'https://images.pexels.com/photos/7089020/pexels-photo-7089020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', aiHint: 'modern medical equipment' },
-    { title: t('kenyamananTitle'), desc: t('kenyamananDesc'), imageUrl: 'https://images.pexels.com/photos/6775268/pexels-photo-6775268.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', aiHint: 'hospital room' },
+    { title: t('kenyamananTitle'), desc: t('kenyamananDesc'), imageUrl: 'https://res.cloudinary.com/ddyqhlilj/image/upload/v1754741672/lobi_y0el0x.jpg', aiHint: 'hospital lobby' },
 ];
 
 export default function HomePage() {
